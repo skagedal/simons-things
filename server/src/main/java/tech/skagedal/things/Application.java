87b329318f1ca18776c36c98e772047402c1a3a3
Simon Kågedal Reimer <skagedal@gmail.com>
2020-08-06 16:10:13 +0200

    Create a simple migration and test some db code

--- server/src/main/java/tech/skagedal/things/Application.java
+++ server/src/main/java/tech/skagedal/things/Application.java
@@ -3,6 +3,8 @@ package tech.skagedal.things;
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -10,9 +12,12 @@ import io.swagger.v3.oas.annotations.info.*;
             version = "0.0"
     )
 )
+
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        final var context = Micronaut.run(Application.class, args);
+        logger.info("Started with environment: {}", context.getEnvironment().getActiveNames());
     }
 }
--- server/src/main/java/tech/skagedal/things/HelloController.java
+++ server/src/main/java/tech/skagedal/things/HelloController.java
@@ -3,14 +3,23 @@ package tech.skagedal.things;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.reactivex.rxjava3.core.Single;
+import io.reactivex.Single;
+import io.vertx.reactivex.mysqlclient.MySQLPool;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Controller("/hello")
 public class HelloController {
 
+    final io.vertx.reactivex.mysqlclient.MySQLPool connection;
+
     Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    public HelloController(MySQLPool connection) {
+        this.connection = connection;
+    }
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() {
@@ -19,8 +28,17 @@ public class HelloController {
     }
 
     @Get("/foo")
-    public Single<Person> getFoo() {
-        return Single.just(new Person("hello"));
+    public Single<String> getFoo() {
+        return connection
+            .query("SELECT @@sql_mode")
+            .rxExecute()
+            .map(rows -> "hello, " + formatRowset(rows));
+    }
+
+    private String formatRowset(io.vertx.reactivex.sqlclient.RowSet<io.vertx.reactivex.sqlclient.Row> rows) {
+        return StreamSupport.stream(rows.spliterator(), false)
+            .map(row -> row.getString(0))
+            .collect(Collectors.joining());
     }
 
     public static class Person {
